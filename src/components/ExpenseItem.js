e8000195707f3e5598d6f4b3e19388e7a9afb35f
Reid McCulloch <rmcc0724@gmail.com>
2021-04-20 23:35:03 -0700

    Expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>April 1st, 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$100</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem

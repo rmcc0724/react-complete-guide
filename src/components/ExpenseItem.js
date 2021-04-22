68fcae3485454d97151bc53eed410881f2da4c40
Reid McCulloch <rmcc0724@gmail.com>
2021-04-22 00:30:08 -0700

    Expense item Map

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>April 1st, 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$100</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   )

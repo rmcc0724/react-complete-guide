dfee83ea60e6a2cc3903f123c582958e3d22718b
Reid McCulloch <rmcc0724@gmail.com>
2021-05-05 02:23:28 -0700

    List filter complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
-  const [setYear, selectedYear] = useState('2020');
+  const [setYear, selectedYear] = useState('2020')
+  const [setExpenses, selectedExpenses] = useState(props.expenses)
 
   const selectedYearListener = (year) => {
-    selectedYear(year);
+    const yearExpenses = props.expenses.filter((i) =>
+      i.date.getFullYear().toString() === year.toString() ? i : null,
+    )
+    const setNewYear = async () =>
+      selectedYear(() => {
+        return year
+      });
+      
+      setNewYear().then(() => {
+      selectedExpenses(()=>{
+        return yearExpenses;
+      })
+    })
   }
 
-
-console.log(setYear);
   return (
-  <div>
-  <Card className='expenses'>
-  <ExpensesFilter selected={setYear} className='expense-filter' onYearChange={selectedYearListener}/>
-{props.expenses.map((i) => (
-    <ExpenseItem
-      key={i.id}
-      title={i.title}
-      amount={i.amount}
-      date={i.date}
-    />))}
-
-</Card>
-  </div>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={setYear}
+          className="expense-filter"
+          onYearChange={selectedYearListener}
+        />
+        {setExpenses.map((i) => (
+          <ExpenseItem
+            key={i.id}
+            title={i.title}
+            amount={i.amount}
+            date={i.date}
+          />
+        ))}
+      </Card>
+    </div>
   )
 }
 export default Expenses

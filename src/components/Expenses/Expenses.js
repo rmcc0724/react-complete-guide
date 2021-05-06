6df0fcc4c48cf6e35b99ad02932b31d6173190c1
Reid McCulloch <rmcc0724@gmail.com>
2021-05-06 00:16:36 -0700

    Adding a conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,13 +7,28 @@ import ExpensesFilter from './ExpensesFilter'
 const Expenses = (props) => {
   const [setYear, selectedYear] = useState('2020')
 
+  ///If the year us changed, update the state with the selected year
   const selectedYearListener = (year) => {
     selectedYear(year)
   }
 
+  ///After the state is updated with the current year, re-render the component and show expenses for the selected year
   const filteredExpenses = props.expenses.filter((i) => {
     return i.date.getFullYear().toString() === setYear
   })
+
+  let expensesContent = <p>No expenses found</p>;
+  
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((i) => (
+      <ExpenseItem
+        key={i.id}
+        title={i.title}
+        amount={i.amount}
+        date={i.date}
+      />
+    ))
+  }
 
   return (
     <div>
@@ -23,14 +38,7 @@ const Expenses = (props) => {
           className="expense-filter"
           onYearChange={selectedYearListener}
         />
-        {filteredExpenses.map((i) => (
-          <ExpenseItem
-            key={i.id}
-            title={i.title}
-            amount={i.amount}
-            date={i.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   )

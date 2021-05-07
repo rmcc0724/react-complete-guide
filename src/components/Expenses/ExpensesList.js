3b9e1dbbab75ef4a95a2bd9e3b9df25893e30e56
Reid McCulloch <rmcc0724@gmail.com>
2021-05-06 19:15:00 -0700

    Cleaned up Separate List

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,20 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-    let expensesContent = <p>No expenses found</p>;
-  
-    if(props.expensesList.length > 0) {
-      expensesContent = props.expensesList.map((i) => (
-        <ExpenseItem
-          key={i.id}
-          title={i.title}
-          amount={i.amount}
-          date={i.date}
-        />
-      ))
-    }
-    return (
-        <div>{expensesContent}</div>
-    )
+      
+    if(props.expensesList.length === 0) {
+        return <h2>No expenses found</h2>; 
+    } else {
+        return (
+        <ul className="expenses-list">
+        {props.expensesList.map((i) => (
+            <ExpenseItem
+              key={i.id}
+              title={i.title}
+              amount={i.amount}
+              date={i.date}
+            />
+          ))}
+          </ul>
+        )}
 }
 export default ExpensesList;
